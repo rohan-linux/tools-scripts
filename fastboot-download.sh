1e2fc785b84e0c04b6b17f79e28affe9ec7ed600
rohan <kjh.rohan@gmail.com>
2024-08-27 13:41:15 +0900

    scripts: fastboot: Add "-d <DEVICE>" option to update to specific device
    Signed-off-by: rohan <kjh.rohan@gmail.com>

--- fastboot-download.sh
+++ fastboot-download.sh
@@ -2,14 +2,13 @@
 #
 
 OUTPUT_DIR="$(realpath $(dirname $(realpath "${BASH_SOURCE}")))"
+FASTBOOT_DEVICE= #"FPGA-AB"
 
 declare -A FASTBOOT_IMAGES=(
 	["DTB"]="  0x80600000 0x0100000 : ${OUTPUT_DIR}/linux.dtb"
 	["LINUX"]="0x81000000 0x1000000 : ${OUTPUT_DIR}/Image"
 	["ROOT"]=" 0x82000000 0x3000000 : ${OUTPUT_DIR}/rootfs.cpio"
 )
-
-FASTBOOT_EXIT="fastboot continue"
 
 function logerr() { echo -e "\033[1;31m$*\033[0m"; }
 function logmsg() { echo -e "\033[0;33m$*\033[0m"; }
@@ -18,36 +17,75 @@ function logext() {
     exit 1
 }
 
+function fastboot_download() {
+	logmsg "Fastboot Download"
+
+	for c in ${!FASTBOOT_IMAGES[*]}; do
+		local ctx="${FASTBOOT_IMAGES[${c}]}"
+		local buf=$(echo ${ctx} | cut -d':' -f1 | sed 's/^[ \t]*//;s/[ \t]*$//;s/\s\s*/ /g')
+		local img=$(echo ${ctx} | cut -d':' -f2 | sed 's/^[ \t]*//;s/[ \t]*$//;s/\s\s*/ /g')
+		local cmd=()
+
+		if [[ ! -f ${img} ]]; then
+			logerr "Not found ${c} : ${img}"
+			continue
+		fi
+
+		logmsg  "[${c}]"
+
+		cmd=( "fastboot" )
+		[[ -n ${FASTBOOT_DEVICE} ]] && cmd+=( "-s ${FASTBOOT_DEVICE}" );
+		cmd+=( "oem run:fastboot_buf ${buf}" )
+
+		logmsg  " Set Buffer"
+		logmsg  " $ ${cmd[*]}"
+		bash -c "${cmd[*]}"
+
+		cmd=( "fastboot" )
+		[[ -n ${FASTBOOT_DEVICE} ]] && cmd+=( "-s ${FASTBOOT_DEVICE}" );
+		cmd+=( "boot ${img}" )
+
+		logmsg  " Download"
+		logmsg  " $ ${cmd[*]}"
+		bash -c "${cmd[*]}"
+
+		logmsg ""
+	done
+}
+
+function fastboot_exit() {
+	logmsg "Fastboot EXIT"
+	local cmd="fastboot continue"
+	logmsg  " $ ${cmd}"
+	bash -c "${cmd}"
+}
+
+function fastboot_usage() {
+	echo " Usage: Please run this script as root or using sudo!"
+	echo -e "\t$ sudo $(basename "${0}") <option>"
+	echo " option:"
+	echo -e "\t-d <DEVICE>\t specify a device $ fastboot devices"
+	echo -e "\t-h\t\t show help"
+	echo ""
+}
+
+function fastboot_args() {
+	while getopts "d:h" opt; do
+		case ${opt} in
+		d) FASTBOOT_DEVICE="${OPTARG}";;
+		h) fastboot_usage
+		   exit 0 ;;
+		*) exit 1 ;;
+		esac
+	done
+}
+
+fastboot_args "${@}"
+
 if [[ $(id -u) -ne 0 ]]; then
 	logext "Please run this script as root or using sudo!"
 fi
 
-logmsg "Fastboot Download"
-for c in ${!FASTBOOT_IMAGES[*]}; do
-	ctx="${FASTBOOT_IMAGES[${c}]}"
-	buf=$(echo ${ctx} | cut -d':' -f1 | sed 's/^[ \t]*//;s/[ \t]*$//;s/\s\s*/ /g')
-	img=$(echo ${ctx} | cut -d':' -f2 | sed 's/^[ \t]*//;s/[ \t]*$//;s/\s\s*/ /g')
+fastboot_download
+fastboot_exit
 
-	if [[ ! -f ${img} ]]; then
-		logerr "Not found ${c} : ${img}"
-		continue
-	fi
-
-	logmsg  "[${c}]"
-	cmd="fastboot oem run:fastboot_buf ${buf}"
-	logmsg  " set buffer"
-	logmsg  " $ ${cmd}"
-	bash -c "${cmd}"
-
-	cmd="fastboot boot ${img}"
-	logmsg  " download"
-	logmsg  " $ ${cmd}"
-	bash -c "${cmd}"
-
-	logmsg ""
-done
-
-logmsg ""
-logmsg "Fastboot EXIT"
-logmsg  " $ ${FASTBOOT_EXIT}"
-bash -c "${FASTBOOT_EXIT}"
