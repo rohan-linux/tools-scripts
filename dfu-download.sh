f4e940d38ddc7fc3d94f200ce54475fe6dc99419
jhk <kjh.rohan@gmail.com>
2025-07-23 14:46:45 +0900

    scripts: download: dd -t option for partial downloads
    This patch introduces the `-t <target>` command-line option to the download script.
    This new option enables partial download functionality, allowing users to specify
    a particular image or set of images to download.
    
    Signed-off-by: jhk <kjh.rohan@gmail.com>

--- dfu-download.sh
+++ dfu-download.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 #
 
-
-DFU_IMAGES=("dxv3_bl2.img" "tfw.img" "bl31.img" "u-boot.img")
-
+DFU_IMAGES=(
+	"dxv3_bl2.img"
+	"bootfw.img"
+	"ddrfw.img"
+	"tfw.img"
+	"bl31.img"
+	"u-boot.img"
+)
 function logerr() { echo -e "\033[1;31m$*\033[0m"; }
 function logmsg() { echo -e "\033[0;33m$*\033[0m"; }
 function logext() {
@@ -13,7 +18,9 @@ function logext() {
 
 DFU_VERBOSE=false
 DFU_SIGN_IMAGE=false
+DFU_LIST_IMAGE=false
 DFU_OUTPUT_PATH="$(pwd)"
+DFU_TARGETS=()
 
 function dfu_download() {
 	logmsg "DFU Wait Connection ..."
@@ -26,7 +33,7 @@ function dfu_download() {
 		fi
 	done
 
-	for img in ${DFU_IMAGES[*]}; do
+	for img in ${DFU_TARGETS[*]}; do
 		img=$(realpath ${DFU_OUTPUT_PATH}/${img})
 		if [[ ! -f ${img} ]]; then
 			logerr "Not found : ${img}"
@@ -64,17 +71,26 @@ function dfu_download() {
 function dfu_usage() {
 	echo -e "\t$ sudo $(basename "${0}") <option>"
 	echo " option:"
-	echo -e "\t-p\t\t set images path"
+	echo -e "\t-t\t\t download targets"
 	echo -e "\t-s\t\t download the signing images"
+	echo -e "\t-l\t\t show download images"
+	echo -e "\t-p\t\t set images path"
 	echo -e "\t-v\t\t verbose"
 	echo -e "\t-h\t\t show help"
 	echo ""
 }
 
-while getopts "p:svh" opt; do
+while getopts "t:p:slvh" opt; do
 	case ${opt} in
+	t)	DFU_TARGETS=("${OPTARG}")
+			until [[ $(eval "echo \${$OPTIND}") =~ ^-.* ]] || [[ -z "$(eval "echo \${$OPTIND}")" ]]; do
+				DFU_TARGETS+=("$(eval "echo \${$OPTIND}")")
+				OPTIND=$((OPTIND + 1))
+			done
+			;;
 	p)	DFU_OUTPUT_PATH=${OPTARG};;
 	s)	DFU_SIGN_IMAGE=true;;
+	l)	DFU_LIST_IMAGE=true;;
 	v)	DFU_VERBOSE=true;;
 	h)	dfu_usage
 		exit 0
@@ -82,5 +98,17 @@ while getopts "p:svh" opt; do
 	*)	exit 1 ;;
 	esac
 done
+
+if [[ ${DFU_LIST_IMAGE} == true ]]; then
+	logmsg "Download Images:"
+	for img in ${DFU_IMAGES[*]}; do
+		logmsg "- ${img}"
+	done
+	exit 0
+fi
+
+if [[ -z ${DFU_TARGETS} ]]; then
+	DFU_TARGETS=(${DFU_IMAGES[@]})
+fi
 
 dfu_download
