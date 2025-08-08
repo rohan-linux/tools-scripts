96262be382c417f82788b41e658d5f8fb98c6990
jhk <kjh.rohan@gmail.com>
2025-08-08 18:35:19 +0900

    scripts: mk_ext4img: Use fakeroot for ext4 image creation
    When the root filesystem is provided as a cpio image, `fakeroot` is now
    utilized during the ext4 image generation process. This ensures that
    file permissions and ownerships are correctly preserved in the resulting
    ext4 filesystem, which is crucial for proper system operation.
    
    Signed-off-by: jhk <kjh.rohan@gmail.com>

--- mk_ext4img.sh
+++ mk_ext4img.sh
@@ -16,7 +16,7 @@ function exec_shell() {
 	exec="$(echo "${exec}" | sed 's/^[ \t]*//;s/[ \t]*$//;s/\s\s*/ /g')"
 	logmsg " $ ${exec}"
 
-	if [[ ${_verbose} == true ]]; then
+	if [[ ${VERBOSE} == true ]]; then
 		bash -c "${exec}"
 		err=${?}
 	else
@@ -32,10 +32,11 @@ function usage() {
 	echo ""
 	echo "option:"
 	echo -e "\t-d\t\troot directory"
+	echo -e "\t-f\t\tcpio image file - Use fakeroot to maintain root permission."
 	echo -e "\t-l\t\tdisk label (optional)"
 	echo -e "\t-s\t\tsize(nG, nM), minimum size is 1M"
-	echo -e "\t-o\t\toutput image name ({output}.ext4, {output}.simg)"
-	echo -e "\t\t\textend: .ext4 = ext4 image, .simg = android sparse image"
+	echo -e "\t-o\t\toutput image name ({output}.ext4, {output}.img)"
+	echo -e "\t\t\textend: .ext4 = ext4 image, .img = android sparse image"
 	echo ""
 }
 
@@ -73,44 +74,56 @@ function htob() {
 	eval "${byte}=\"${value}\""
 }
 
-function gen_image_ext4() {
+function gen_ext4() {
 	local ext4img aospimg # android sparse image
 	local count
 	local bs="1M"
 	local command=()
 
-	if [[ ! -d ${_image_path} ]]; then
-		logext "No such image directory: ${_image_path}"
+	if [[ ! -d ${IMAGE_DIR} && ! -f ${IMAGE_FILE} ]]; then
+		logerr "No such image directory '${IMAGE_DIR}' or file '${IMAGE_FILE}'"
+		usage
+		exit 1
 	fi
 
-	if [[ -z ${_image_output} ]]; then
-		_image_output=${_image_path}
+	if [[ -z ${IMAGE_SIZE} || -z ${IMAGE_OUT} ]]; then
+		logerr "Not defined output name or ext4 size !!!"
+		usage
+		exit 1
 	fi
 
-	ext4img="${_image_output}.ext4"
-	aospimg="${_image_output}.simg"
+	ext4img="${IMAGE_OUT}.ext4"
+	aospimg="${IMAGE_OUT}.img"
 
-	htob ${_image_size} count
+	htob ${IMAGE_SIZE} count
 	count=$((count / 1024 / 1024))
-	logmsg "Make EXT4: ${ext4img} and ${aospimg}, size ${_image_size}byte"
+	logmsg " Make EXT4: ${ext4img} and ${aospimg}, size ${IMAGE_SIZE}byte"
 
-	command=("dd"
-		"if=/dev/zero"
-		"of=${ext4img}"
-		"bs=${bs}"
-		"count=${count}"
-		"conv=sparse")
-	exec_shell "${command[*]}"
-	if [[ $? -ne 0 ]]; then
-		logext " - FAILED"
-	fi
+	if [[ -d ${IMAGE_DIR} ]]; then
+		command=("dd" "if=/dev/zero" "of=${ext4img}" "bs=${bs}" "count=${count}" "conv=sparse")
+		exec_shell "${command[*]}"
+		if [[ $? -ne 0 ]]; then
+			logext " - FAILED"
+		fi
 
-	command=("mkfs.ext4")
-	[[ -n ${_image_label} ]] && command+=("-L ${_image_label}")
-	command+=("-d ${_image_path}" "${ext4img}")
-	exec_shell "${command[*]}"
-	if [[ ! -f ${ext4img} ]]; then
-		logext " - FAILED"
+		command=("mkfs.ext4")
+		[[ -n ${IMAGE_LABEL} ]] && command+=("-L ${IMAGE_LABEL}")
+		command+=("-d ${IMAGE_DIR}" "${ext4img}")
+		exec_shell "${command[*]}"
+		if [[ ! -f ${ext4img} ]]; then
+			logext " - FAILED"
+		fi
+	else
+		fakeroot sh -c "
+			mkdir -p rootfs
+			cd rootfs
+			cpio -idmv < ../${IMAGE_FILE} >/dev/null 2>&1
+			cd ..
+			dd if=/dev/zero of=${IMAGE_OUT}.ext4 bs=1M count=${count}
+			mkfs.ext4 -d rootfs ${IMAGE_OUT}.ext4"
+		if [[ ! -f ${ext4img} ]]; then
+			logext " - FAILED"
+		fi
 	fi
 
 	command=("img2simg" "${ext4img}" "${aospimg}")
@@ -120,20 +133,22 @@ function gen_image_ext4() {
 	fi
 }
 
-_image_path=""
-_image_label=""
-_image_size=""
-_image_output=""
-_verbose=false
+IMAGE_DIR=""
+IMAGE_FILE="" # cpio image
+IMAGE_LABEL=""
+IMAGE_SIZE=""
+IMAGE_OUT=""
+VERBOSE=false
 
-function gen_image_args() {
-	while getopts "d:l:s:o:vh" opt; do
+function gen_args() {
+	while getopts "d:f:l:s:o:vh" opt; do
 		case ${opt} in
-		d)	_image_path="${OPTARG}" ;;
-		l)	_image_label="${OPTARG}" ;;
-		s)	_image_size="${OPTARG}" ;;
-		o)	_image_output="${OPTARG}" ;;
-		v)	_verbose=true ;;
+		d)	IMAGE_DIR="${OPTARG}" ;;
+		f)	IMAGE_FILE="${OPTARG}" ;;
+		l)	IMAGE_LABEL="${OPTARG}" ;;
+		s)	IMAGE_SIZE="${OPTARG}" ;;
+		o)	IMAGE_OUT="${OPTARG}" ;;
+		v)	VERBOSE=true ;;
 		h)	usage
 			exit 0
 			;;
@@ -142,5 +157,5 @@ function gen_image_args() {
 	done
 }
 
-gen_image_args "${@}"
-gen_image_ext4
+gen_args "${@}"
+gen_ext4
